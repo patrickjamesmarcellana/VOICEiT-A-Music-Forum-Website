a17beaf917115902916294376f5b29bfa21f8e77
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-06-21 16:02:20 +0000

    Added Default Forum Info for Side Panel B

--- js/index.js
+++ js/index.js
@@ -1,4 +1,73 @@
 $(document).ready(function() {
+   // changing nav bar button when logged in 
+   const nav_buttons_panel = $(".nav-buttons-panel");
+   const nav_button = $(".nav-button");
+   const user_buttons = $(".user-buttons");
+
+   const logged_in = false; // default value of website: user is logged out
+   const username = `mrdoggo`;
+   const profile_photo = `images/empty-profile.png`;
+   if(logged_in) {
+       nav_button.remove();
+       user_buttons.remove();
+       nav_buttons_panel.append(`
+           <span class="user-buttons dropdown">
+               <button class="dropdown-button">
+                   <img class="icon nav-icons" src="images/user.png">
+                   <i class="arrow down"></i>
+               </button>
+                       
+               <div class="dropdown-content options-panel">
+                   <br>
+                   <span class="side-panel-text">USER OPTIONS</span>
+                   <a class="side-panel-a-buttons" href="profile.html">
+                       <span class="profile-sprite"></span>
+                       Profile
+                   </a>
+                   <a class="side-panel-a-buttons" href="index.html">
+                       <span class="logout-sprite"></span>
+                       Logout
+                   </a>
+               </div>
+           </span>
+           <a class="post-profile nav-profile" href="profile.html">
+               <img class="post-profile-photo" src="${profile_photo}">
+           </a>
+           <a class="post-profile nav-profile-name" href="profile.html">
+               ${username}
+           </a>
+           <button class="logout-button">
+               <span class="logout-sprite">
+           </button>   
+       `)
+   } else {
+       nav_button.remove();
+       user_buttons.remove();
+       nav_buttons_panel.append(`
+       <span class="user-buttons dropdown">
+           <button class="dropdown-button">
+               <img class="icon nav-icons" src="images/user.png">
+               <i class="arrow down"></i>
+           </button>
+           
+           <div class="dropdown-content options-panel">
+               <br>
+               <span class="side-panel-text">USER OPTIONS</span>
+               <a class="side-panel-a-buttons" href="register.html">
+                   <span class="register-sprite"></span>
+                   Register
+               </a>
+               <a class="side-panel-a-buttons" href="Register.html">
+                   <span class="login-sprite"></span>
+                   Login
+               </a>
+           </div>
+       </span>
+       <a class="register-button nav-button" href="register.html">Register</a>
+       <a class="login-button nav-button" href="register.html">Log-in</a>
+       `);
+   }
+
 
     // for dynamic content sizing when adjusting window size
     const post_panel = $(".post-panel");
@@ -60,73 +129,6 @@ $(document).ready(function() {
     })
 
 
-    // changing nav bar button when logged in 
-    const nav_buttons_panel = $(".nav-buttons-panel");
-    const nav_button = $(".nav-button");
-    const user_buttons = $(".user-buttons");
-
-    const logged_in = false; // default value of website: user is logged out
-    /* sample values for variables */
-    const username = `mrdoggo`;
-    const profile_photo = `images/empty-profile.png`;
-    if(logged_in) {
-        nav_button.remove();
-        user_buttons.remove();
-        nav_buttons_panel.append(`
-            <span class="user-buttons dropdown">
-                <button class="dropdown-button">
-                    <img class="icon nav-icons" src="images/user.png">
-                    <i class="arrow down"></i>
-                </button>
-                        
-                <div class="dropdown-content options-panel">
-                    <br>
-                    <span class="side-panel-text">USER OPTIONS</span>
-                    <a class="side-panel-a-buttons" href="profile.html">
-                        <span class="profile-sprite"></span>
-                        Profile
-                    </a>
-                    <a class="side-panel-a-buttons" href="index.html">
-                        <span class="logout-sprite"></span>
-                        Logout
-                    </a>
-                </div>
-            </span>
-            <a class="post-profile nav-profile" href="profile.html">
-                <img class="post-profile-photo" src="${profile_photo}">
-            </a>
-            <a class="post-profile nav-profile-name" href="profile.html">
-                ${username}
-            </a>
-            <button class="logout-button">
-                <span class="logout-sprite">
-            </button>   
-        `)
-    } else {
-        nav_button.remove();
-        user_buttons.remove();
-        nav_buttons_panel.append(`
-        <span class="user-buttons dropdown">
-            <button class="dropdown-button">
-                <img class="icon nav-icons" src="images/user.png">
-                <i class="arrow down"></i>
-            </button>
-            
-            <div class="dropdown-content options-panel">
-                <br>
-                <span class="side-panel-text">USER OPTIONS</span>
-                <a class="side-panel-a-buttons" href="register.html">
-                    <span class="register-sprite"></span>
-                    Register
-                </a>
-                <a class="side-panel-a-buttons" href="Register.html">
-                    <span class="login-sprite"></span>
-                    Login
-                </a>
-            </div>
-        </span>
-        <a class="register-button nav-button" href="register.html">Register</a>
-        <a class="login-button nav-button" href="register.html">Log-in</a>
-        `);
-    }
+    // event listeners for side panel a buttons (changing the forum info on side panel b)
+    
 })
