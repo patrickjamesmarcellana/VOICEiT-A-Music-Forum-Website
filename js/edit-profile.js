524132e348c3bb07537e31e92b4de85550d3d4e9
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2023-06-23 12:27:34 +0800

    profile: Clear edit profile modal inputs when closed

--- js/edit-profile.js
+++ js/edit-profile.js
@@ -5,11 +5,15 @@ const editProfileBtns = document.querySelectorAll(".edit-profile-button");
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        document.querySelector("#profile-picture").value = "";
+        document.querySelector("#description").value = "";
     }
 };
 
 exitModalBtn.addEventListener("click", (event) => {
     modal.style.display = "none";
+    document.querySelector("#profile-picture").value = "";
+    document.querySelector("#description").value = "";
 });
 
 for (const editProfileBtn of editProfileBtns) {
