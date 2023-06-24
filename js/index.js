e728050513961a0f3eb8fefa0981bb4aed2a1e90
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-06-24 15:59:27 +0000

    Encoded user profiles

--- js/index.js
+++ js/index.js
@@ -183,7 +183,7 @@ $(document).ready(function() {
     })
 
     
-    
+    // changing subforums
     function Forum(name, description) {
         this.name = name;
         this.description = description;
@@ -280,6 +280,7 @@ $(document).ready(function() {
     function changeForum(forum_id) {
         // are we in index.html
         if(window.location.pathname.split("/").pop() == "index.html") {
+            //window.location.href = `index.html?forum=${forum_id}`;
             forum_name.text(forums[forum_id].name);
             forum_description.html(forums[forum_id].description);
         } else {
@@ -301,6 +302,56 @@ $(document).ready(function() {
         $("." + forum_id).click(function() {
             changeForum(forum_id_copy);
         })
+    }
+
+    function User(username, description) {
+        this.username = username;
+        this.description = description;
+    }
+
+    // hardcoded user profiles 
+    const users = {
+        melissa_spellman: new User(`melissa_spellman`, `
+            Hi! I'm a neuroscientist student from Johns Hopkins University, and I'm highly invested in how music transcends
+            generations. Currently, my favorite genres of music are classical, jazz, and R&B. Hit me up on my personal email
+            if you'd like to discuss music on your leisure time!
+
+            Email: melissa_spellman@yahoo.com
+        `),
+        draeznor_rock_lover: new User(`draeznor_rock_lover`, `
+            I've been an avid fan of rock and hiphop music since the 1990s. Red Hot Chili Peppers (a rock band) made me
+            become a fan of their music, oh I love them! Eventually, I became a fan of the genre itself.
+        `),
+        jennie_itgirl: new User(`jennie_itgirl`, `
+            An honest kpop fan. Jennie Ruby Jane my idol <3. Posts about kpop and Blackpink in general, but I also love
+            posting about other music genres.
+        `),
+        marithus_25: new User(`marithus_25`, `
+            Country music is such an underrated genre. Here to spread country music to fellow music listeners! I have also 
+            been exploring latin music, and Bad Bunny is such an amazing artist.
+        `),
+        aria_eagleheart: new User(`aria_eagleheart`, ``)
+    };
+
+    const URL_USER_KEY = "user";
+    const profile_username = $(".profile-username");
+    const user_description = $(".user-description");
+
+    function changeProfile(user_id) {
+        // are we in profile.html
+        if(window.location.pathname.split("/").pop() == "profile.html") {
+            //window.location.href = `index.html?user=${username}`;
+            profile_username.text(users[user_id].username);
+            user_description.text(users[user_id].description);
+        } else {
+            window.location.href = `index.html?user=${user_id}`
+        }
+    }
+
+    // should we change the profile
+    const goto_user_id = search_params.get(URL_USER_KEY);
+    if(goto_user_id != null) {
+        changeProfile(goto_user_id);
     }
 
     // making the title panel (page title + logo) clickable to redirect to home page
