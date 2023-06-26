c1502be70ee87d8efbfd30d27f36aff0a764f29a
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-06-26 19:45:57 +0800

    Missed

--- js/profile.js
+++ js/profile.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     const posts_list = Object.entries(posts).filter((kvpair) => (kvpair[1].op === target_user));
     const comments_list = Object.entries(saved_comments).filter((kvpair) => (kvpair[1].author === target_user));
     for (let [post_id, post] of posts_list) {
-        insert_post(post_id, post)
+        insert_post(post_id)
     }
     
     for (let [comment_id, comment] of comments_list) {
