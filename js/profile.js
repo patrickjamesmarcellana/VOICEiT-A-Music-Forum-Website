01c6cde25d0c9fb5a289aa590c332d2c4910cc36
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-06-26 20:16:50 +0800

    New profile view

--- js/profile.js
+++ js/profile.js
@@ -5,13 +5,21 @@ $(document).ready(function() {
     // note: .op is for post, .author is for comment (fix this)
     // note 2: post is a global variable containing all the posts for some reason
     // note 3: same for saved_comments
-    const posts_list = Object.entries(posts).filter((kvpair) => (kvpair[1].op === target_user));
+    const post_id_list = Object.keys(posts).filter((post_id) => (posts[post_id].op === target_user));
+    
     const comments_list = Object.entries(saved_comments).filter((kvpair) => (kvpair[1].author === target_user));
-    for (let [post_id, post] of posts_list) {
+
+    // find post the comment belongs to
+    for (let [comment_id, comment] of comments_list) {
+        if(comment.post_id != null && !post_id_list.includes(comment.post_id)) { // todo: optimize
+            post_id_list.push(comment.post_id);
+        }
+    }
+    for (let post_id of post_id_list) {
         insert_post(post_id)
     }
     
     for (let [comment_id, comment] of comments_list) {
-        renderComment(comment_id, document.querySelector(".post-panel"))
+        renderComment(comment_id, document.querySelector(`div[post-id="${comment.post_id}"]`), undefined, ["dont-render-subcomments"])
     }
 })
