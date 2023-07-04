9434122d76708b8be532ade8209e1cb017bcdaa9
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-07-04 20:56:01 +0800

    Fix photos

--- mongodb_populate.js
+++ mongodb_populate.js
@@ -24,31 +24,31 @@ async function test() {
     await (new User({
         username: "melissa_spellman",
         description: "Hi! I'm a neuroscientist student from Johns Hopkins University, and I'm highly invested in how music transcends generations. Currently, my favorite genres of music are classical, jazz, and R&B. Hit me up on my personal email if you'd like to discuss music on your leisure time!\n\nEmail: melissa_spellman@yahoo.com",
-        photoUrl: "images/melissa_spellman.png"
+        photoUrl: "images/melissa_spellman.jpg"
     })).save()
     
     await (new User({
         username: "draeznor_rock_lover",
         description: "I've been an avid fan of rock and hiphop music since the 1990s. Red Hot Chili Peppers (a rock band) made me become a fan of their music, oh I love them! Eventually, I became a fan of the genre itself.",
-        photoUrl: "images/draeznor_rock_lover.png"
+        photoUrl: "images/draeznor_rock_lover.jpg"
     })).save()
 
     await (new User({
         username: "jennie_itgirl",
         description: "An honest kpop fan. Jennie Ruby Jane my idol <3. Posts about kpop and Blackpink in general, but I also love posting about other music genres.",
-        photoUrl: "images/jennie_itgirl.png"
+        photoUrl: "images/jennie_itgirl.jpg"
     })).save()
 
     await (new User({
         username: "marithus_25",
         description: "Country music is such an underrated genre. Here to spread country music to fellow music listeners! I have also been exploring latin music, and Bad Bunny is such an amazing artist.",
-        photoUrl: "images/marithus_25.png"
+        photoUrl: "images/marithus_25.jpg"
     })).save()
 
     await (new User({
         username: "aria_eagleheart",
         description: "aria_eagleheart",
-        photoUrl: "images/aria_eagleheart.png"
+        photoUrl: "images/aria_eagleheart.jpg"
     })).save()
     console.log("Populated \"users\" table")
 
