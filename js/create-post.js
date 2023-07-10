2b95948f7dfa11e73a81f390fb14e0f06d38fd4f
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-07-10 21:55:28 +0800

    Delete duplicate (3)

--- js/create-post.js
+++
@@ -1,4 +0,0 @@
-$(".post-title-input").on("input", function() {
-    this.css("height", "auto");
-    this.css("height", this.scrollHeight + "px");
-});
--- js/edit-post.js
+++
@@ -1,4 +0,0 @@
-//eventlistener for clicking the edit button (redirects to edit-post.html)
-$(".edit-post-button").click(function(e) {
-    window.location.href = "edit-post.html?post=" + e.currentTarget.closest(".post-container").getAttribute("post-id");
-})
