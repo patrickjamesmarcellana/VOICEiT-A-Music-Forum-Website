6a38ccbefd66e20a7c5fab0c325aaf6d13b069b2
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-07-13 17:31:13 +0800

    Live comment count

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -1,12 +1,13 @@
 const router = require("express").Router()
 
+const Comment = require("../models/Comment")
 const Post = require("../models/Post")
 const User = require("../models/User")
 
 // remove private info, set public info etc
 // and get comments list
 // TODO: explore https://www.mongodb.com/docs/manual/reference/operator/aggregation/lookup/#pipe._S_lookup
-const documentToJson = (document) => { 
+const documentToJson = async (document) => { 
     console.log(document)
     return {
         post_id: document._id,
@@ -16,16 +17,16 @@ const documentToJson = (document) => {
         title: document.title,
         text: document.body,
         date: document.date,
-        comment_count: -1,
+        comment_count: await Comment.count({post_id: document._id}),
         top_level_comments_list: document.top_level_comments_list
     }
 }
 
-const documentsToJson = (documents) => {
+const documentsToJson = async (documents) => {
     const json = []
     
     for(let i = 0; i < documents.length; i++) {
-        json.push(documentToJson(documents[i]))
+        json.push(await documentToJson(documents[i]))
     }
     return json
 }
@@ -34,7 +35,7 @@ router.get("/id/:id", async (req, res) => {
     try{
         let query = await Post.findById(req.params.id).populate("user")
         console.log(query)
-        const json = [documentToJson(query)]
+        const json = [await documentToJson(query)]
     
         if(json != null) {
             res.send(json)
@@ -51,7 +52,7 @@ router.get("/user/:user", async (req, res) => {
     try {
         let user_id = await User.findOne({username: req.params.user})
         let query = await Post.find({user: user_id}).populate("user")
-        let json = documentsToJson(query)
+        let json = await documentsToJson(query)
         
         if(json != null) {
             res.send(json)
@@ -75,7 +76,7 @@ router.get("/subforum/:subforum", async (req, res) => {
 
     try {
         let query = await Post.find().populate("user")
-        let json = documentsToJson(query)
+        let json = await documentsToJson(query)
         
         //hardcoded_posts_list.forEach(post => post.comment_count = comment_count(post.top_level_comments_list))
         if(req.params.subforum === "home") {
