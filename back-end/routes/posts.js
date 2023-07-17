1c4dafc0e186661d74a5cbcfaa88094fb59ad03a
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-07-17 16:44:47 +0800

    Add batch loading support (experimental)

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -8,7 +8,7 @@ const User = require("../models/User")
 // and get comments list
 // TODO: explore https://www.mongodb.com/docs/manual/reference/operator/aggregation/lookup/#pipe._S_lookup
 const documentToJson = async (document) => { 
-    console.log(document)
+    //console.log(document)
     return {
         post_id: document._id,
         subforum: document.subforum,
@@ -74,7 +74,28 @@ router.get("/subforum/:subforum", async (req, res) => {
 
     try {
         let query = []
-        let cursor
+        // let cursor
+
+        //TODO: fix this before Sat Sep 13 275760 08:00:00 GMT+0800 (Philippine Standard Time)
+        let last_sent_views = 2 ^ 64 // reasonable max num of views
+        let last_sent_datetime = new Date(8640000000000000) // max date supported by JS
+        let last_sent_id = 2 ^ 96 // larger than max id (2^96 - 1)
+        let post_limit = 0 // no limit
+
+        // if the front-end knows the last post it was sent
+        // we can skip all posts that were posted earlier than that
+        if(req.query.last_sent_views) {
+            last_sent_views = req.query.last_sent_views
+        }
+        if(req.query.last_sent_datetime) {
+            last_sent_datetime = new Date(req.query.last_sent_datetime)
+        }
+        if(req.query.last_sent_id) {
+            last_sent_id = new Date(req.query.last_sent_id)
+        }
+        if(req.query.post_limit) {
+            post_limit = req.query.post_limit
+        }
         
         //hardcoded_posts_list.forEach(post => post.comment_count = comment_count(post.top_level_comments_list))
         if(req.params.subforum === "home") {
@@ -85,11 +106,62 @@ router.get("/subforum/:subforum", async (req, res) => {
             // }
             
             // for now: retrieve all then handle per-batch load in front end
-            query = await Post.find().sort({date: -1}).populate("user").exec()
+
+            // UPDATE: attempt to do it without cursors
+            // inspiration: https://medium.com/swlh/mongodb-pagination-fast-consistent-ece2a97070f3
+            // sort by date field
+            // tie-breaker in the case the posts have the same date/time: post id
+
+            // note: we could use object ID only to sort the posts (since first 4 bytes of it represents the time in seconds) but
+            //   1. the sample post's document creation time (eg. the time when the document was inserted to the DB) !=  post's actual creation date (eg. the hardcoded time of the post)
+            //   2. the object ID will overflow in the year 2106
+
+            query = await Post.find({
+                $or: [
+                    {
+                        date: { $lt: last_sent_datetime },
+                    },
+                    {
+                        date: last_sent_datetime,
+                        id: {$lt: last_sent_id }
+                    }
+                ]
+            }).sort({date: -1, _id: -1}).limit(post_limit).populate("user").exec() // DEBUG: to dump the actual query process, add .explain() before .exec()
+
+            // DEBUG: in the dump, make sure the word SORT does not appear 
+            // for performance reasons, we should not be sorting every time we query
+            //console.log(query.queryPlanner)
         } else if(req.params.subforum === "popular"){
-            query = await Post.find().sort({views: -1}).populate("user").exec()
+            query = await Post.find({
+                $or: [
+                    {
+                        views: { $lt: last_sent_views },
+                    },
+                    {
+                        views: last_sent_views,
+                        id: {$lt: last_sent_id }
+                    }
+                ]
+            }).sort({views: -1, _id: -1}).limit(post_limit).populate("user").exec()
         } else {
-            query = await Post.find({subforum: req.params.subforum}).populate("user").exec()
+            query = await Post.find({
+                $and: [
+                    {
+                        $or: [
+                        {
+                            date: { $lt: last_sent_datetime },
+                        },
+                        {
+                            date: last_sent_datetime,
+                            id: {$lt: last_sent_id }
+                        }
+                        ],
+                    }, 
+                    {
+                        subforum: req.params.subforum,
+                    },
+                ]
+            }).sort({date: -1, _id: -1}).limit(post_limit).populate("user").exec()
         }
         
         const json = await documentsToJson(query)
@@ -111,7 +183,7 @@ router.get("/search/:searchkey", async(req, res) => {
         // How to Populate Users with aggregate?
 
         const query = await Post.aggregate([
-            { $match: { $text: { $search: key } } },
+            { $match: { $text: { $search: key, $language: "none" } } },
             { $sort: { score: { $meta: "textScore" } } }
         ])
 
