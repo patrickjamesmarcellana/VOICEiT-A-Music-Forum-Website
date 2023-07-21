57ad5296b75f61e2181d9d7623d0f8585415a62f
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2023-07-22 07:58:22 +0800

    frontend/Register: refactor JS functions

--- front-end/js/register-script.js
+++ front-end/js/register-script.js
@@ -3,71 +3,36 @@ const loginPassword = document.getElementById("login-password");
 const confirmLoginPassword = document.getElementById("confirm-login-password");
 
 loginUsername.addEventListener("keyup", (event) => {
-    const invalidUsername = document.createElement("div");
-    invalidUsername.className = "invalid-field-message";
-    invalidUsername.id = "invalid-username-message";
-    invalidUsername.innerHTML =
-        "Username must be between 3 - 20 characters only. It \
-        can contain the following: uppercase, lowercase, numbers, and underscores.";
-
     // if username is invalid, set text box border to red and display message
     // if one is not displayed already
     if (!isUsernameFormatValid()) {
         loginUsername.style.border = "2px solid red";
-        const invalidUsernameMessage = document.getElementById(
-            "invalid-username-message"
-        );
-        if (!invalidUsernameMessage) {
-            loginUsername.parentElement.append(invalidUsername);
-        }
+        addInvalidUsernameMessage();
         return;
     }
 
     // if username is valid, set text box border to default and remove invalid
     // message if there is any
     loginUsername.style.border = "";
-    const invalidUsernameMessage = document.getElementById(
-        "invalid-username-message"
-    );
-    if (invalidUsernameMessage) {
-        invalidUsernameMessage.remove();
-    }
+    removeInvalidUsernameMessage();
 });
 
 loginPassword.addEventListener("keyup", (event) => {
     // update confirm password error message as well
     setMismatchedPasswordMessage();
 
-    const invalidPassword = document.createElement("div");
-    invalidPassword.className = "invalid-field-message";
-    invalidPassword.id = "invalid-password-message";
-    invalidPassword.innerHTML =
-        "Password must be between 8 - 32 characters long. It should \
-        contain one of each: uppercase, lowercase, numbers, and symbols \
-        (#?!@$%^&*-).";
-
     // if password is invalid, set text box border to red and display message
     // if one is not displayed already
     if (!isPasswordFormatValid()) {
         loginPassword.style.border = "2px solid red";
-        const invalidPasswordMessage = document.getElementById(
-            "invalid-password-message"
-        );
-        if (!invalidPasswordMessage) {
-            loginPassword.parentElement.append(invalidPassword);
-        }
+        addInvalidPasswordMessage();
         return;
     }
 
     // if password is valid, set text box border to default and remove invalid
     // message if there is any
     loginPassword.style.border = "";
-    const invalidPasswordMessage = document.getElementById(
-        "invalid-password-message"
-    );
-    if (invalidPasswordMessage) {
-        invalidPasswordMessage.remove();
-    }
+    removeInvalidPasswordMessage();
 });
 
 confirmLoginPassword.addEventListener("keyup", (event) => {
@@ -89,27 +54,96 @@ function arePasswordsMatching() {
     return loginPassword.value === confirmLoginPassword.value;
 }
 
-function setMismatchedPasswordMessage() {
-    const mismatchedPassword = document.createElement("div");
-    mismatchedPassword.className = "invalid-field-message";
-    mismatchedPassword.id = "mismatched-confirm-password-message";
-    mismatchedPassword.innerHTML = "Entered password does not match.";
+function addInvalidUsernameMessage() {
+    const invalidUsername = document.createElement("div");
+    invalidUsername.className = "invalid-field-message";
+    invalidUsername.id = "invalid-username-message";
+    invalidUsername.innerHTML =
+        "Username must be between 3 - 20 characters only. It \
+        can contain the following: uppercase, lowercase, numbers, and underscores.";
+    
+    const invalidUsernameMessage = document.getElementById(
+        "invalid-username-message"
+    );
+    if (!invalidUsernameMessage) {
+        loginUsername.parentElement.append(invalidUsername);
+    }
+}
 
+function removeInvalidUsernameMessage() {
+    document.getElementById("invalid-username-message")?.remove();
+}
+
+function addInvalidPasswordMessage() {
+    const invalidPassword = document.createElement("div");
+    invalidPassword.className = "invalid-field-message";
+    invalidPassword.id = "invalid-password-message";
+    invalidPassword.innerHTML =
+        "Password must be between 8 - 32 characters long. It should \
+        contain one of each: uppercase, lowercase, numbers, and symbols \
+        (#?!@$%^&*-).";
+    
+    const invalidPasswordMessage = document.getElementById(
+        "invalid-password-message"
+    );
+    if (!invalidPasswordMessage) {
+        loginPassword.parentElement.append(invalidPassword);
+    }
+}
+
+function removeInvalidPasswordMessage() {
+    document.getElementById("invalid-password-message")?.remove();
+}
+
+function setMismatchedPasswordMessage() {
     // if entered password does not match, set text border to red and
     // display message if one is not displayed already
     if (!arePasswordsMatching()) {
         confirmLoginPassword.style.border = "2px solid red";
-        const mismatchedConfirmPasswordMessage = document.getElementById(
-            "mismatched-confirm-password-message"
-        );
-        if (!mismatchedConfirmPasswordMessage) {
-            confirmLoginPassword.parentElement.append(mismatchedPassword);
-        }
+        addMismatchedPasswordMessage();
         return;
     }
 
     // if password matches, set text box border to default and remove
     // invalid message if there is any
     confirmLoginPassword.style.border = "";
+    removeMismatchedPasswordMessage();
+}
+
+function addMismatchedPasswordMessage() {
+    const mismatchedPassword = document.createElement("div");
+    mismatchedPassword.className = "invalid-field-message";
+    mismatchedPassword.id = "mismatched-confirm-password-message";
+    mismatchedPassword.innerHTML = "Entered passwords do not match.";
+
+    const mismatchedConfirmPasswordMessage = document.getElementById(
+        "mismatched-confirm-password-message"
+    );
+    if (!mismatchedConfirmPasswordMessage) {
+        confirmLoginPassword.parentElement.append(mismatchedPassword);
+    }
+}
+
+function removeMismatchedPasswordMessage() {
     document.getElementById("mismatched-confirm-password-message")?.remove();
+}
+
+function addUsernameExistsMessage() {
+    const usernameTaken = document.createElement("div");
+    usernameTaken.className = "invalid-field-message";
+    usernameTaken.id = "existing-username-message";
+    usernameTaken.innerHTML = "Username is already taken!";
+
+    const usernameTakenMessage = document.getElementById(
+        "existing-username-message"
+    );
+    loginPassword.style.border = "2px solid red";
+    if (!usernameTakenMessage) {
+        loginUsername.parentElement.append(usernameTaken);
+    }
+}
+
+function removeUsernameExistsMessage() {
+    loginUsername.style.border = "";
+    document.getElementById("existing-username-message")?.remove();
 }
