3c959561e2e67b6ab4a454510f2e1f352e1a19ea
Latasha Harwani <115960087+Tasha1403@users.noreply.github.com>
2023-07-22 21:57:43 +0800

    updated with richtext

--- front-end/js/create-post.js
+++ front-end/js/create-post.js
@@ -2,3 +2,163 @@ $(".post-title-input").on("input", function() {
     this.css("height", "auto");
     this.css("height", this.scrollHeight + "px");
 });
+
+$(document).ready(function() {
+    $('#post-content').richText({
+        // text formatting
+        bold: true,
+        italic: true,
+        underline: true,
+
+        // text alignment
+        leftAlign: true,
+        centerAlign: true,
+        rightAlign: true,
+        justify: true,
+
+        // lists
+        ol: true,
+        ul: true,
+
+        // title
+        heading: false,
+
+        // fonts
+        fonts: true,
+        fontList: [
+            "Arial", 
+            "Arial Black", 
+            "Comic Sans MS", 
+            "Courier New", 
+            "Geneva", 
+            "Georgia", 
+            "Helvetica", 
+            "Impact", 
+            "Lucida Console", 
+            "Tahoma", 
+            "Times New Roman",
+            "Verdana"
+        ],
+        fontColor: false,
+        backgroundColor: false,
+        fontSize: false,
+
+        // uploads
+        imageUpload: false,
+        fileUpload: false,
+
+        // media
+        videoEmbed: false,
+
+        // link
+        urls: true,
+
+        // tables
+        table: false,
+
+        // code
+        removeStyles: true,
+        code: false,
+
+        // colors
+        colors: [],
+
+        // dropdowns
+        fileHTML: '',
+        imageHTML: '',
+
+        // translations
+        translations: {
+            'title': 'Title',
+            'white': 'White',
+            'black': 'Black',
+            'brown': 'Brown',
+            'beige': 'Beige',
+            'darkBlue': 'Dark Blue',
+            'blue': 'Blue',
+            'lightBlue': 'Light Blue',
+            'darkRed': 'Dark Red',
+            'red': 'Red',
+            'darkGreen': 'Dark Green',
+            'green': 'Green',
+            'purple': 'Purple',
+            'darkTurquois': 'Dark Turquois',
+            'turquois': 'Turquois',
+            'darkOrange': 'Dark Orange',
+            'orange': 'Orange',
+            'yellow': 'Yellow',
+            'imageURL': 'Image URL',
+            'fileURL': 'File URL',
+            'linkText': 'Link text',
+            'url': 'URL',
+            'size': 'Size',
+            'responsive': 'Responsive',
+            'text': 'Text',
+            'openIn': 'Open in',
+            'sameTab': 'Same tab',
+            'newTab': 'New tab',
+            'align': 'Align',
+            'left': 'Left',
+            'justify': 'Justify',
+            'center': 'Center',
+            'right': 'Right',
+            'rows': 'Rows',
+            'columns': 'Columns',
+            'add': 'Add',
+            'pleaseEnterURL': 'Please enter an URL',
+            'videoURLnotSupported': 'Video URL not supported',
+            'pleaseSelectImage': 'Please select an image',
+            'pleaseSelectFile': 'Please select a file',
+            'bold': 'Bold',
+            'italic': 'Italic',
+            'underline': 'Underline',
+            'alignLeft': 'Align left',
+            'alignCenter': 'Align centered',
+            'alignRight': 'Align right',
+            'addOrderedList': 'Ordered list',
+            'addUnorderedList': 'Unordered list',
+            'addHeading': 'Heading/title',
+            'addFont': 'Font',
+            'addFontColor': 'Font color',
+            'addBackgroundColor': 'Background color',
+            'addFontSize': 'Font size',
+            'addImage': 'Add image',
+            'addVideo': 'Add video',
+            'addFile': 'Add file',
+            'addURL': 'Add URL',
+            'addTable': 'Add table',
+            'removeStyles': 'Remove styles',
+            'code': 'Show HTML code',
+            'undo': 'Undo',
+            'redo': 'Redo',
+            'close': 'Close',
+            'save': 'Save'
+        },
+                    
+        // privacy
+        youtubeCookies: false,
+            
+        // preview
+        preview: false,
+
+        // placeholder
+        placeholder: 'Voice It!',
+        
+        // developer settings
+        useSingleQuotes: false,
+        height: 0,
+        heightPercentage: 0,
+        adaptiveHeight: false,
+        id: "",
+        class: "",
+        useParagraph: false,
+        maxlength: 0,
+        maxlengthIncludeHTML: false,
+        callback: undefined,
+        useTabForNext: false,
+        save: false,
+        saveCallback: undefined,
+        saveOnBlur: 0, 
+        undoRedo: true
+    });
+});
