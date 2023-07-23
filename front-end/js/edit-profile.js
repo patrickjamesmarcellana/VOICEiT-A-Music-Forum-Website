61aed2ba657592d5c0c1fad8430eaf80a191a18a
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-07-23 23:37:11 +0800

    Revert back remove photo functionality

--- front-end/js/edit-profile.js
+++ front-end/js/edit-profile.js
@@ -17,33 +17,24 @@ exitModalBtn.addEventListener("click", (event) => {
     document.querySelector("#description").value = "";
 });
 
-// removePictureBtn.addEventListener("click", async (event) => {
-//     event.preventDefault();
+removePictureBtn.addEventListener("click", async (event) => {
+    event.preventDefault();
 
-//     try {
-//         const response = await fetch("/api/submit/edit-profile?_method=PATCH", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({
-//                 removePhoto: "true",
-//             }),
-//         });
+    try {
+        const response = await fetch("/api/submit/edit-profile/remove-photo", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
 
-//         if (response.status === 200) {
-//             window.location.reload();
-//         }
-//     } catch (err) {
-//         console.error(err);
-//     }
-// });
-
-$("#remove-picture").click(() => {
-    const new_image = document.getElementById("newimage");
-    new_image.src = 'images/empty-profile.png'
-})
-
+        if (response.status === 200) {
+            window.location.reload();
+        }
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 for (const editProfileBtn of editProfileBtns) {
     editProfileBtn.addEventListener("click", async (event) => {
