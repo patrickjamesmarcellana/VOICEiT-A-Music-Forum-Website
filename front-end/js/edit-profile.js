f75259e740adab9e4729745089d98c4866a9e202
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2023-07-24 08:56:30 +0800

    Edit Profile: refactor JS script

--- front-end/js/edit-profile.js
+++ front-end/js/edit-profile.js
@@ -2,6 +2,13 @@ const modal = document.querySelector(".edit-profile-modal");
 const exitModalBtn = document.querySelector(".exit-button");
 const editProfileBtns = document.querySelectorAll(".edit-profile-button");
 const removePictureBtn = document.querySelector("#remove-picture");
+const submitBtn = document.querySelector("#submit-button");
+
+// when file is selected, change image preview
+const changePhoto = (event) => {
+    const newImage = document.getElementById("newimage");
+    newImage.src = URL.createObjectURL(event.target.files[0]);
+};
 
 window.onclick = function (event) {
     if (event.target == modal) {
@@ -31,50 +38,52 @@ removePictureBtn.addEventListener("click", async (event) => {
 
 for (const editProfileBtn of editProfileBtns) {
     editProfileBtn.addEventListener("click", async (event) => {
-        const search_params = new URLSearchParams(window.location.search)
-        user = await userManager.getUser(search_params.get("user"));
+        // set data to modal
+        const search_params = new URLSearchParams(window.location.search);
+        const user = await userManager.getUser(search_params.get("user"));
+        const newImage = document.querySelector("#newimage");
         document.querySelector("#description").value = user.description;
-        document.querySelector("#newimage").src = user.photoUrl;
-        modal.style.display = "block";
-        if($("#newimage").attr("src") === "images/empty-profile.png") {
-            $("#remove-picture").attr('disabled', 'disabled')
-            $("#remove-picture").addClass('noHover')
+        newImage.src = user.photoUrl;
+
+        // disable removing of picture when profile is default
+        if (newImage.src === "images/empty-profile.png") {
+            const removePictureBtn = document.querySelector("#remove-picture");
+            removePictureBtn.disabled = true;
+            removePictureBtn.classList.add("noHover");
         }
+
+        // display the modal
+        modal.style.display = "block";
     });
 }
 
-const changePhoto = (event) => {
-    const new_image = document.getElementById("newimage");
-    new_image.src = URL.createObjectURL(event.target.files[0]);
-};
+submitBtn.addEventListener("click", async (event) => {
+    event.preventDefault();
 
-$("#submit-button").click(async (e) => {
-    e.preventDefault()
-    const search_params = new URLSearchParams(window.location.search)
-    const username = search_params.get("user")
+    const search_params = new URLSearchParams(window.location.search);
+    const username = search_params.get("user");
+    const description = document.querySelector("#description").value;
+    const inputFile = document.querySelector("#file").files[0];
+    const formData = new FormData();
 
-    const description = $("#description").val()
-    const input_file = $("#file").get(0).files[0]
-    const formData = new FormData()
-    
-    formData.set('description', description)
-    formData.set('file', input_file)
+    formData.set("description", description);
+    formData.set("file", inputFile);
 
-    if(!input_file) {
-        try {
-            await userManager.editDescOnly(username, description)
-            modal.style.display = "none"
-            window.location.reload()
-        } catch(err) {
-            console.error(err)
+    console.log(formData.get("description"));
+    console.log(formData.get("file"));
+
+    try {
+        if (!inputFile) {
+            // if user selected no file, then retain profile photo
+            await userManager.editDescOnly(username, description);
+        } else {
+            // otherwise, change both their profile photo and description
+            await userManager.editProfile(username, formData);
         }
-    } else {
-        try {
-            await userManager.editProfile(username, formData)
-            modal.style.display = "none"
-            window.location.reload()
-        } catch(err) {
-            console.error(err)
-        }
+
+        modal.style.display = "none";
+        window.location.reload();
+    } catch (error) {
+        console.error(error);
     }
-})
+});
